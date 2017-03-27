d4545962172264cbab6a64f78c72bf30972f8649
nangelov1990 <nangelov1990@gmail.com>
2017-03-27 15:39:07 +0300

    ASPNET-Essentials exercise - Car dealer app

---
+++ 02/CarDealerApp-Skeleton/CarDealer.Data/Migrations/Configuration.cs
@@ -0,0 +1,19 @@
+namespace CarDealer.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CarDealer.Data.CarDealerContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CarDealer.Data.CarDealerContext context)
+        {
+        }
+    }
+}
