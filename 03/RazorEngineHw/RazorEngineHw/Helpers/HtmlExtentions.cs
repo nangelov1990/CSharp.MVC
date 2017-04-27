edcc8ee52c0bf97d46d79b6a690e3a927c4365fe
Nick Angelov <nangelov1990@gmail.com>
2017-04-27 19:31:41 +0300

    Exercises: Razor Engine (8)

---
+++ 03/RazorEngineHw/RazorEngineHw/Helpers/HtmlExtentions.cs
@@ -0,0 +1,28 @@
+﻿namespace RazorEngineHw.Helpers
+{
+    using System.Web.Mvc;
+
+    public static class HtmlExtentions
+    {
+        public static MvcHtmlString Image(
+            this HtmlHelper helper,
+            string imageUrl,
+            string alt = null,
+            string width = "150px",
+            string height = "150px")
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.MergeAttribute("src", imageUrl);
+            builder.MergeAttribute("width", width);
+            builder.MergeAttribute("height", height);
+            if (alt != null)
+            {
+                builder.MergeAttribute("alt", alt);
+            }
+
+            MvcHtmlString htmlString = new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+
+            return htmlString;
+        }
+    }
+}
