50a1f265859e332444f6118566172391b6f7ecad
Nick Angelov <nangelov1990@gmail.com>
2017-04-27 19:51:07 +0300

    Exercises: Razor Engine (9)

--- 03/RazorEngineHw/RazorEngineHw/Helpers/HtmlExtentions.cs
+++ 03/RazorEngineHw/RazorEngineHw/Helpers/HtmlExtentions.cs
@@ -4,6 +4,8 @@
 
     public static class HtmlExtentions
     {
+        private static readonly string _youtubeUrl = "https://youtube.com/embed/";
+
         public static MvcHtmlString Image(
             this HtmlHelper helper,
             string imageUrl,
@@ -21,6 +23,29 @@
             }
 
             MvcHtmlString htmlString = new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+
+            return htmlString;
+        }
+
+        public static MvcHtmlString Youtube(
+            this HtmlHelper helper,
+            string videoId,
+            int width = 600,
+            int height = 400,
+            bool allowfullscreen = true)
+        {
+            var fullSource = $"{_youtubeUrl}{videoId}";
+            TagBuilder builder = new TagBuilder("iframe");
+            builder.MergeAttribute("src", fullSource);
+            builder.MergeAttribute("width", $"{width}");
+            builder.MergeAttribute("height", $"{height}");
+            builder.MergeAttribute("frameborder", "0");
+            if (allowfullscreen == true)
+            {
+                builder.MergeAttribute("allowfullscreen", "allowfullscreen");
+            }
+
+            MvcHtmlString htmlString = new MvcHtmlString(builder.ToString(TagRenderMode.Normal));
 
             return htmlString;
         }
